bc887ec80b492e5b6905578b9e8c8b8b1d42d0cb
StijnvanDoornum <stijn@vandulken.net>
2025-05-13 12:39:41 +0200

    made training files modular and added agents to init file

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,5 +1,6 @@
 from agents.base_agent import BaseAgent
 from .qlearning_agent import QLearningAgent
-
+from .mc_agent import MCAgent
+from .value_iteration import ValueIterationAgent
 
 __all__ = ["BaseAgent"]
